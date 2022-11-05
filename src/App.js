6890f43f134e96f48b521ba7ea3424a660bba808
Mohammad Mahmud Hasan <34007876+mahmud92542@users.noreply.github.com>
2022-11-05 11:41:01 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello AWS!
+          Hello All!
           Welcome to AWS Amplify!
         </a>
       </header>
